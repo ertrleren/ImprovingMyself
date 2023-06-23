412d5a4e63f5b3c327ddde24703c66ac7a297385
Ertuğrul Eren <ertrleren@gmail.com>
2023-06-23 17:04:47 +0300

    ImprovingMyself
    work

---
+++ Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //var result = Add2();
+
+            //int number1 = 20;
+            //int number2 = 100;
+            //var result2 = Add3(number1,number2);
+            //Console.WriteLine(result2);
+
+            //Console.WriteLine(Multiply(2, 4));
+            //Console.WriteLine(Multiply(2, 4, 5));
+            Console.WriteLine(Add4(2, 3, 4, 5, 6, 7, 8, 4));
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        static int Add2(int number1 = 20, int number2 = 30)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+        static int Add3(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2;
+        }
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum(); 
+        }
+    }
+}
